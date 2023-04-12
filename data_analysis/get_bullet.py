083b5b5be3e55b28b2531ad52bd01d2eeffb05af
wangsq <127467920+wangsqd@users.noreply.github.com>
2023-04-12 18:22:21 +0800

    Add files via upload

---
+++ data_analysis/get_bullet.py
@@ -0,0 +1,10 @@
+import re
+import requests
+
+url = input('请输入B站视频链接: ')
+res = requests.get(url)
+cid = re.findall(r'"cid":(.*?),', res.text)[-1]
+url = f'https://comment.bilibili.com/{cid}.xml'
+res = requests.get(url)
+with open(f'{cid}.xml', 'wb') as f:
+    f.write(res.content)
---
+++ data_analysis/to_csv.py
@@ -0,0 +1,16 @@
+# processing.py
+import re
+
+with open('test.xml', encoding='utf-8') as f:
+    data = f.read()
+
+comments = re.findall('<d p="(.*?)">(.*?)</d>', data)
+# print(len(comments))  # 3000
+danmus = [','.join(item) for item in comments]
+headers = ['stime', 'mode', 'size', 'color', 'date', 'pool', 'author', 'dbid', 'text']
+headers = ','.join(headers)
+danmus.insert(0, headers)
+
+with open('danmus.csv', 'w', encoding='utf_8_sig') as f:
+    f.writelines([line+'\n' for line in danmus])
+
